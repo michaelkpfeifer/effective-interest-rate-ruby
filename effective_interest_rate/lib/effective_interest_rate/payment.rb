a06dec1ef0ecdf1445f056427f75087f09e32a40
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2016-08-28 16:01:49 +0200

    Add a very first trivial test

--- effective_interest_rate/lib/effective_interest_rate/payment.rb
+++ effective_interest_rate/lib/effective_interest_rate/payment.rb
@@ -1,2 +1,9 @@
 class Payment
+  attr_accessor :amount
+  attr_accessor :date
+
+  def initialize(amount, date)
+    @amount = amount
+    @date = date
+  end
 end
---
+++ effective_interest_rate/test/payment_test.rb
@@ -0,0 +1,8 @@
+require 'test/unit'
+require 'effective_interest_rate'
+
+class PaymentTest < Test::Unit::TestCase
+  def test_the_truth
+    assert_equal 0, 0
+  end
+end
