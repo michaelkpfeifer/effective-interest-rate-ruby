ddeb07555209db14739dd6f9633061142aa7d4a9
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2016-09-19 16:55:20 +0200

    Add class and tests for payment list

--- effective_interest_rate/lib/effective_interest_rate/payment_list.rb
+++ effective_interest_rate/lib/effective_interest_rate/payment_list.rb
@@ -1,2 +1,23 @@
-class PaymentSequence
+class PaymentList
+  attr_accessor :payment_list
+
+  def initialize
+    @payment_list = []
+  end
+
+  def size
+    payment_list.size
+  end
+
+  def <<(payment)
+    @payment_list << payment
+  end
+
+  def min_date
+    @payment_list.sort.first.date
+  end
+
+  def max_date
+    @payment_list.sort.last.date
+  end
 end
---
+++ effective_interest_rate/test/test_payment_list.rb
@@ -0,0 +1,32 @@
+require 'test/unit'
+require 'effective_interest_rate'
+
+class PaymentListTest < Test::Unit::TestCase
+  def test_add
+    payment_list = PaymentList.new
+    assert_equal payment_list.size, 0
+
+    payment1 = Payment.new(1000, Date.new(2011, 1, 1))
+    payment2 = Payment.new(1000, Date.new(2012, 1, 1))
+    payment3 = Payment.new(1000, Date.new(2013, 1, 1))
+
+    payment_list << payment1
+    assert_equal payment_list.size, 1
+    payment_list << payment2
+    assert_equal payment_list.size, 2
+    payment_list << payment3
+    assert_equal payment_list.size, 3
+  end
+
+  def test_order
+    payment_list = PaymentList.new
+    payment1 = Payment.new(1000, Date.new(2011, 1, 1))
+    payment2 = Payment.new(1000, Date.new(2012, 1, 1))
+    payment3 = Payment.new(1000, Date.new(2013, 1, 1))
+    payment_list << payment2
+    payment_list << payment3
+    payment_list << payment1
+    assert_equal payment_list.min_date, Date.new(2011, 1, 1)
+    assert_equal payment_list.max_date, Date.new(2013, 1, 1)
+  end
+end
