1744b35caa43bdb1979d27f1f95b22b9626e18b2
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2016-09-24 18:25:43 +0200

    Improve naming of local variables

--- test/test_payment.rb
+++ test/test_payment.rb
@@ -3,24 +3,24 @@ require 'effective_interest_rate'
 
 class PaymentTest < Test::Unit::TestCase
   def test_order
-    p1 = Payment.new(1000, Date.new(2011, 1, 1))
-    p2 = Payment.new(1000, Date.new(2012, 1, 1))
+    payment1 = Payment.new(1000, Date.new(2011, 1, 1))
+    payment2 = Payment.new(1000, Date.new(2012, 1, 1))
 
-    assert_operator p1, :<, p2
-    assert_operator p2, :>, p1
-    assert_operator p1, :<=, p2
-    assert_operator p2, :>=, p1
-    assert_operator p1, :<=, p1
-    assert_operator p2, :>=, p2
+    assert_operator payment1, :<, payment2
+    assert_operator payment2, :>, payment1
+    assert_operator payment1, :<=, payment2
+    assert_operator payment2, :>=, payment1
+    assert_operator payment1, :<=, payment1
+    assert_operator payment2, :>=, payment2
   end
 
   def test_sort
-    p1 = Payment.new(1000, Date.new(2011, 1, 1))
-    p2 = Payment.new(1000, Date.new(2012, 1, 1))
-    p3 = Payment.new(1000, Date.new(2013, 1, 1))
+    payment1 = Payment.new(1000, Date.new(2011, 1, 1))
+    payment2 = Payment.new(1000, Date.new(2012, 1, 1))
+    payment3 = Payment.new(1000, Date.new(2013, 1, 1))
 
-    assert_equal [p1, p2, p3].sort.reverse.first, p3
-    assert_equal [p1, p2, p3].min, p1
-    assert_equal [p1, p2, p3].max, p3
+    assert_equal [payment1, payment2, payment3].sort.reverse.first, payment3
+    assert_equal [payment1, payment2, payment3].min, payment1
+    assert_equal [payment1, payment2, payment3].max, payment3
   end
 end
