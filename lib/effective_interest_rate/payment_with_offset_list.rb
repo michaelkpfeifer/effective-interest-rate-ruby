9ff4df1db6f320f704d97b4347489896e4f9d61c
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2016-10-16 23:03:28 +0200

    Refactor #initialize

--- lib/effective_interest_rate/payment_with_offset_list.rb
+++ lib/effective_interest_rate/payment_with_offset_list.rb
@@ -3,37 +3,21 @@ class PaymentWithOffsetList
   attr_accessor :payment_with_offset_list_derivative
 
   def initialize(payment_with_date_list)
-    @payment_with_offset_list = []
-    @payment_with_offset_list_derivative = []
-
     first_payment_date = payment_with_date_list.min_date
-
-    if first_payment_date.leap?
-      first_payment_with_offset = (first_payment_date.yday - 1) / 366.0
-    else
-      first_payment_with_offset = (first_payment_date.yday - 1) / 365.0
-    end
+    first_payment_offset = payment_offset_in_year(first_payment_date)
     first_payment_year = first_payment_date.year
 
+    @payment_with_offset_list = []
     payment_with_date_list.payment_with_date_list.each do |payment_with_date|
       payment_with_date_date = payment_with_date.date
       year_difference = payment_with_date_date.year - first_payment_year
-      if payment_with_date_date.leap?
-        offset = (payment_with_date_date.yday - 1) / 366.0
-      else
-        offset = (payment_with_date_date.yday - 1) / 365.0
-      end
-      offset = offset + year_difference - first_payment_with_offset
+      offset = payment_offset_in_year(payment_with_date_date)
+      offset = offset + year_difference - first_payment_offset
       payment_with_offset = PaymentWithOffset.new(payment_with_date.amount, offset)
-
       @payment_with_offset_list << payment_with_offset
     end
 
-    @payment_with_offset_list.each do |payment_with_offset|
-      amount = payment_with_offset.amount
-      offset = payment_with_offset.offset
-      @payment_with_offset_list_derivative << PaymentWithOffset.new(-1 * offset * amount, - (- offset - 1))
-    end
+    @payment_with_offset_list_derivative = payment_with_offset_list_derivative(@payment_with_offset_list)
   end
 
   def size
@@ -65,5 +49,24 @@ class PaymentWithOffsetList
       value = value + amount * (1 + x)**-offset
     end
     value
+  end
+
+  def payment_offset_in_year(date)
+    if date.leap?
+      payment_offset = (date.yday - 1) / 366.0
+    else
+      payment_offset = (date.yday - 1) / 365.0
+    end
+    payment_offset
+  end
+
+  def payment_with_offset_list_derivative(payment_with_offset_list)
+    derivative = []
+    payment_with_offset_list.each do |payment_with_offset|
+      amount = payment_with_offset.amount
+      offset = payment_with_offset.offset
+      derivative << PaymentWithOffset.new(-1 * offset * amount, - (- offset - 1))
+    end
+    derivative
   end
 end
