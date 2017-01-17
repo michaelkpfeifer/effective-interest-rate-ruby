6381da7ceb42a6626f227c38363eefe3b52cf9f0
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2017-01-17 21:56:51 +0100

    Get rid of byebug dependency

--- lib/effective_interest_rate.rb
+++ lib/effective_interest_rate.rb
@@ -4,4 +4,4 @@ require 'effective_interest_rate/payment_with_date.rb'
 require 'effective_interest_rate/payment_with_offset.rb'
 require 'effective_interest_rate/effective_interest_rate_calculator.rb'
 
-require 'pry-byebug'
+# require 'pry-byebug'
